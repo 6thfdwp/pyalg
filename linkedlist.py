b04d5517d4332ac4c176ef6563a9f6397ed6c9b1
weiping <weiping@bneing.com>
2014-08-30 00:51:10 +1000

    logic for delete and insert

--- linkedlist.py
+++ linkedlist.py
@@ -1,16 +1,41 @@
 import collections
 
 class Node():
+    """
+     Node representation in a singly linked list
+    """
     def __init__(self, value):
+        """
+         Constructor
+         Initialize with value and None next pointer
+
+         value -- any type of value containing actual data
+        """
         self.value = value
-        #self.prev = None
         self.next = None
 
     def __repr__(self):
         return '%s(%r)' % ('Node', self.value)
 
 class LinkedList(collections.MutableSequence):
+    """
+     Singly linked list class
+     Provide index based operations as built-in list
+
+     Example:
+     linkedlist[0]        : get the first el 
+     linkedlist[5] = value: set the 6th el with new value
+     del linkedlist[3]    : delete the 4th el
+    """
     def __init__(self, iterable=None):
+        """
+        Initialize 3 attributes
+        _len  -- Interger for the number of elements in LinkedList
+        _head -- Pointer to the first elements
+        _tail -- Pointer to the last elements
+
+        @param iterable -- Any iterable object(list, set etc.) to initialize the LinkedList
+        """
         self._len = 0
         self._head = None
         self._tail = None
@@ -18,6 +43,12 @@ class LinkedList(collections.MutableSequence):
             self += iterable
 
     def __repr__(self):
+        """
+        String representation for this linked list
+
+        Example:
+        Node(1)->Node(2)...
+        """
         node = self._head
         _list = []
         while node:
@@ -27,20 +58,27 @@ class LinkedList(collections.MutableSequence):
         return '->'.join(_list)
 
     def _getnode(self, index):
+        """
+        Iterate the linked list to get a Node for the given index
+
+        @param index -- Integer for index
+        """
         if self._head is None: # empty
+            return None
+        if index >= self._len: # index out of range
             return None
         node = self._head
         i = 0
         while i < index:
             node = node.next
-            if node is None: # index out of range
-                return None
+            #if node is None: 
+                #return None
             i += 1
         return node
 
     """
-      Protocol for collections
-      Provide index based operations on LinkedList object
+      Protocol for MutableSequence
+      Provide index based operations
     """
     def __len__(self):
         return self._len
@@ -50,18 +88,24 @@ class LinkedList(collections.MutableSequence):
         node = self._getnode(index)
         node.value = value
     def __delitem__(self, index):
-        node = self._getnode(index)
-        if node is None:
-            return
-        if node == self._head:
-            self._head = node.next
+        if index  == 0: # splice out the first, the second becomes the new head now
+            self._head = self[0].next
         else:
             pre = self._getnode(index-1)
-            pre.next = node.next
+            node = self._getnode(index)
+            # splice out the node by removing the pointer to it
+            pre.next = node.next 
             if node == self._tail:
                 self._tail = pre
 
+        self._len -= 1
+
     def append(self, value):
+        """
+        Append a new Node to the end of the linked list
+
+        @param value -- value for creating a new Node
+        """
         newnode = Node(value)
         if self._head is None:
             self._head = newnode
@@ -72,13 +116,27 @@ class LinkedList(collections.MutableSequence):
         self._len += 1
 
     def insert(self, index, value):
-        node = self._getnode(index)
-        if node is None:
-            return
+        """
+        Insert a new Node at a given index
+
+        @param index -- Integer for a position
+        @param value -- value for the new Node object
+        """
         newnode = Node(value)
-        temp = node.next
-        node.next = newnode
-        newnode.next = temp
+        if index == 0: # at the beginning
+            newnode.next = self._head
+            self._head = newnode
+            return
+        elif index == len(self): # at the end, same as append
+            self._tail.next = newnode
+            self._tail = newnode
+        else:
+            node = self._getnode(index-1)
+            if node is None:
+                return
+            temp = node.next
+            node.next = newnode
+            newnode.next = temp
 
         self._len += 1
 
@@ -109,12 +167,15 @@ class LinkedList(collections.MutableSequence):
         self._head = prev
 
 if __name__ == '__main__':
-    iterable = [i for i in range(10)]
-    ll = LinkedList(iterable)
-    print ll
-    ll.reverse_()
-    print ll
+    #iterable = [i for i in range(10)]
+    #ll = LinkedList(iterable)
+    #print ll
+    #ll.reverse_()
+    #print ll
 
-    ll.append('nnnn')
-    print ll
-    #ll.append(15)
+    import timeit
+    sp = 'from linkedlist import *; ll = LinkedList(range(1000000))'
+    print timeit.repeat("ll.insert(0, 9)", setup=sp, number=5)
+
+    print timeit.repeat("l.insert(0, 9)", setup='l = range(1000000)', number=5)
+

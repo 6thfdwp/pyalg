a26bd7b55c2aebdb0e9cb4621a09b40aca3908ed
weiping <weiping@bneing.com>
2014-09-18 23:27:23 +1000

    int power function comparison

---
+++ int_pow.py
@@ -0,0 +1,88 @@
+"""
+ Different way to implement integer based power function
+ See discussions here:
+ http://stackoverflow.com/questions/101439/the-most-efficient-way-to-implement-an-integer-based-power-function-powint-int
+"""
+
+def pow_(base, exp):
+    """
+    Time:  O(exp)
+    Space: O(1) since use xrange
+    """
+    res = 1
+    for i in xrange(exp):
+        res = base * res
+    return res
+
+def pow_rec(base, exp):
+    """
+    Recurse by one
+
+    Time:  O(exp)
+           Similar to iteration, actual running time will be bigger since involves function call
+    Space: O(exp)
+           exp <= 1000 otherwise will exceed the maximum depth of recursive call
+    """
+    if exp == 0:
+        return 1
+    elif exp == 1:
+        return base
+    return base * pow_rec(base, exp-1)
+
+def pow_rec_2(base, exp):
+    """ 
+    Recurse by half
+    x^n = x^(n/2) * x^(n/2) if n is even else x^n = x * x^(n/2) * x^(n/2)
+
+    Time:  O(log(exp)) 
+           Dominated by the depth of recursive tree, each level takes O(1) to compute
+    Space: O(log(exp)) 
+           used for recursive call stack
+    """
+    if exp == 0:
+        return 1
+    elif exp == 1:
+        return base
+
+    res = pow_rec_2(base, exp>>1)
+    #if exp % 2 == 0:
+    if (exp & 1 == 0): # if it is even
+        return res * res
+    else:
+        return base * res * res
+
+def pow_by_squaring(x, n):
+    """
+    Exponentiation by squaring
+    Time:   O(logn)
+    Space:  O(1)
+    """
+    res = 1
+    while n:
+        if (n & 1): # if it is odd
+            # this will be at least executed once when n == 1
+            res *= x
+        n = n >> 1 # halve the exp
+        x *= x     # ((x^2) ^2) ^2 ...
+    return res
+
+if __name__ == '__main__':
+    import timeit
+    def verbose(size, secs, loops):
+        print 'exponent %d:' % size,
+        print 'total %.3f sec in %d loops' % (secs, loops)
+
+    base, exp = 5, 156666
+    loops, repts = 100, 3
+    print pow_(base, 9)
+    print pow_rec_2(base, 9)
+    print pow_by_squaring(base, 9)
+
+    #secs1 = timeit.timeit("pow_(%d,%d)" % (base,exp), setup="from __main__ import pow_", number=loops)
+    secs2 = timeit.timeit("pow_rec_2(%d,%d)" % (base,exp), setup="from __main__ import pow_rec_2", number=loops)
+    secs3 = timeit.timeit("pow_by_squaring(%d,%d)" % (base,exp), setup="from __main__ import pow_by_squaring", number=loops)
+    #verbose(exp, secs1, loops)
+    verbose(exp, secs2, loops)
+    verbose(exp, secs3, loops)
+
+

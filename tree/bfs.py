5fe5f2cdeb4a24ec037633f501754ff509129433
Weiping <6thfdwp@gmail.com>
2019-06-25 23:51:24 +1000

    bfs test

--- tree/bfs.py
+++ tree/bfs.py
@@ -32,7 +32,9 @@ def find_flight(flights, src, dst, k):
         # extract node with lowest cost so far
         cost, s, stops = heapq.heappop(PQ)
         print cost, s, stops
-
+        # when dst is popped and check how many steps from src to dst 
+        # if <=k stop processing queue, otherwise continue to find higher cost
+        # but explored before (closer to src)
         if s == dst and stops <= k:
             res = (cost, s, stops)
             break
@@ -86,6 +88,7 @@ def explore_maze(maze, start, end):
 
     Q = deque([start])
     # (r, c) tuple is hashable, the equality is based on value of r and c
+    # no matter we create a new tuple for every cell state, they are equal as long as r,c is the same
     visited = set()
     paths = {} # {cell: [parent, depth]}
     while Q:
@@ -114,7 +117,8 @@ if __name__ == '__main__':
     maze = [ [1,1,1,1,1], [1,1,'X',1,1], [1,1,1,1,1], ['X',1,1,1,1], [1,'X',1,1,'X'] ]
     # start, end = (4,3), (1,0),  #(3,3)
     # print explore_maze(maze, start, end)
-    flights = [[0,1,100],[1,2,100],[0,2,500]]
+    flights = [[0,1,100],[1,2,100],[1,3,10],[3,0,10],[3,2,50],[0,2,500]]
+    flights = [[]]
     flights = [[10,14,43],[1,12,62],[4,2,62],[14,10,49],[9,5,29],[13,7,53],[4,12,90],[14,9,38],[11,2,64],[2,13,92],[11,5,42],[10,1,89],[14,0,32],[9,4,81],[3,6,97],[7,13,35],[11,9,63],[5,7,82],[13,6,57],[4,5,100],[2,9,34],[11,13,1],[14,8,1],[12,10,42],[2,4,41],[0,6,55],[5,12,1],[13,3,67],[3,13,36],[3,12,73],[7,5,72],[5,6,100],[7,6,52],[4,7,43],[6,3,67],[3,1,66],[8,12,30],[8,3,42],[9,3,57],[12,6,31],[2,7,10],[14,4,91],[2,3,29],[8,9,29],[2,11,65],[3,8,49],[6,14,22],[4,6,38],[13,0,78],[1,10,97],[8,14,40],[7,9,3],[14,6,4],[4,8,75],[1,6,56]]
     # find_flight(flights, 0, 2, 0)
     find_flight(flights, 1, 4, 5)
